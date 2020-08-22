9f265edf795a9827cdc75f7901867cba30649eea
iromashko <iromashko@me.com>
2020-08-23 02:32:49 +0300

    submit form

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -3,5 +3,12 @@
 describe('Test Contact Us form via Automation Test Store', () => {
   it('Submit form success', () => {
     cy.visit('https://www.automationteststore.com/');
+    cy.get('.info_links_footer > :nth-child(5) > a').click();
+    cy.get('#ContactUsFrm_first_name').type('Joe');
+    cy.get('#ContactUsFrm_email').type('joe@email.com');
+    cy.get('#ContactUsFrm_enquiry').type(
+      'lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    );
+    cy.get('.col-md-6 > .btn').click();
   });
 });
