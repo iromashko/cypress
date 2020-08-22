743b8561ca22ce6873b0b56894b830e9f20f2bb4
iromashko <iromashko@me.com>
2020-08-22 03:56:32 +0300

    submit form error

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -1,13 +1,19 @@
 /// <reference types="Cypress" />
 
 describe('Test Contact Us form via WebdriverUni', () => {
-  it('Submit form', () => {
+  it('Submit form success', () => {
     cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
-    // cy.get('#contact-us').click();
     cy.get('[name="first_name"]').type('Joe');
     cy.get('[name="last_name"]').type('Doe');
     cy.get('[name="email"]').type('example@email.com');
     cy.get('textarea.feedback-input').type('Lorem ipsum dolor amet');
+    cy.get('[type="submit"]').click();
   });
-  it('Invalid Submission', () => {});
+  it('Submit form error', () => {
+    cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+    cy.get('[name="first_name"]').type('Joe');
+    cy.get('[name="last_name"]').type('Doe');
+    cy.get('textarea.feedback-input').type('Lorem ipsum dolor amet');
+    cy.get('[type="submit"]').click();
+  });
 });
