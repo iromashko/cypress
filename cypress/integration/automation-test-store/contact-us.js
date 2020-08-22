8fdf2f8bd3ce32383a2a6e52b4403a45bda883c3
iromashko <iromashko@me.com>
2020-08-22 04:17:16 +0300

    add automation test store

---
+++ cypress/integration/automation-test-store/contact-us.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+
+describe('Test Contact Us form via Automation Test Store', () => {
+  it('Submit form success', () => {
+    cy.visit('https://www.automationteststore.com/');
+  });
+});
