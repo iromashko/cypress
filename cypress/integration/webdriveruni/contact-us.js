8621d3d56f6f644989ac618e822fb106c0a20c03
iromashko <iromashko@me.com>
2020-09-11 15:50:54 +0300

    remove target attribute

--- cypress/integration/webdriveruni/contact-us.js
+++ cypress/integration/webdriveruni/contact-us.js
@@ -2,7 +2,8 @@
 
 describe('Test Contact Us form via WebdriverUni', () => {
   it('Submit form success', () => {
-    cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+    cy.visit('http://www.webdriveruniversity.com');
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true });
     cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
     cy.title().should('include', 'WebDriver | Contact Us');
     cy.url().should('include', 'contactus');
@@ -14,7 +15,8 @@ describe('Test Contact Us form via WebdriverUni', () => {
     cy.get('h1').should('have.text', 'Thank You for your Message!');
   });
   it('Submit form error', () => {
-    cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+    cy.visit('http://www.webdriveruniversity.com/');
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true });
     cy.get('[name="first_name"]').type('Joe');
     cy.get('[name="last_name"]').type('Doe');
     cy.get('textarea.feedback-input').type('Lorem ipsum dolor amet');
