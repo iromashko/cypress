70a1382a1549ec2b48e015981064ea968aa67e53
iromashko <iromashko@me.com>
2020-09-13 01:12:19 +0300

    use fixtures

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -1,15 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Test Contact Us form via Automation Test Store', () => {
-  it('Submit form success', () => {
+  before(function () {
+    cy.fixture('example').then(function (data) {
+      this.data = data;
+    });
+  });
+  it('Submit form success', function () {
     cy.visit('https://www.automationteststore.com/');
     cy.get("a[href$='contact']")
       .click()
       .then(function (linkText) {
         cy.log('Clicked link ' + linkText.text());
       });
-    cy.get('#ContactUsFrm_first_name').type('Joe');
-    cy.get('#ContactUsFrm_email').type('joe@email.com');
+    cy.get('#ContactUsFrm_first_name').type(this.data.first_name);
+    cy.get('#ContactUsFrm_first_name').type(this.data.first_name);
+    cy.get('#ContactUsFrm_email').type(this.data.email);
     cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email');
     cy.get('#ContactUsFrm_enquiry').type(
       'lorem ipsum dolor sit amet, consectetur adipiscing elit'
--- cypress/integration/webdriveruni/contact-us.js
+++ cypress/integration/webdriveruni/contact-us.js
@@ -1,25 +1,30 @@
 /// <reference types="Cypress" />
 
 describe('Test Contact Us form via WebdriverUni', () => {
-  it('Submit form success', () => {
+  beforeEach(function () {
+    cy.fixture('example').then(function (data) {
+      this.data = data;
+    });
+  });
+  it('Submit form success', function () {
     cy.visit('http://www.webdriveruniversity.com');
     cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true });
     cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
     cy.title().should('include', 'WebDriver | Contact Us');
     cy.url().should('include', 'contactus');
-    cy.get('[name="first_name"]').type('Joe');
-    cy.get('[name="last_name"]').type('Doe');
-    cy.get('[name="email"]').type('example@email.com');
-    cy.get('textarea.feedback-input').type('Lorem ipsum dolor amet');
+    cy.get('[name="first_name"]').type(this.data.first_name);
+    cy.get('[name="last_name"]').type(this.data.last_name);
+    cy.get('[name="email"]').type(this.data.email);
+    cy.get('textarea.feedback-input').type(this.data.body);
     cy.get('[type="submit"]').click();
     cy.get('h1').should('have.text', 'Thank You for your Message!');
   });
-  it('Submit form error', () => {
+  it('Submit form error', function () {
     cy.visit('http://www.webdriveruniversity.com/');
     cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true });
-    cy.get('[name="first_name"]').type('Joe');
-    cy.get('[name="last_name"]').type('Doe');
-    cy.get('textarea.feedback-input').type('Lorem ipsum dolor amet');
+    cy.get('[name="first_name"]').type(this.data.first_name);
+    cy.get('[name="last_name"]').type(this.data.last_name);
+    cy.get('textarea.feedback-input').type(this.data.body);
     cy.get('[type="submit"]').click();
     cy.get('body').contains('Error: all fields are required');
   });
