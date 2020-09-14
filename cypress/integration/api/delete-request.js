237751c63e77c6b86b13463a913936b170bace2c
iromashko <iromashko@me.com>
2020-09-15 02:08:36 +0300

    api challenge

---
+++ cypress/integration/api/delete-request.js
@@ -0,0 +1,12 @@
+/// <reference types='Cypress' />
+
+describe('Delete Request', () => {
+  it('Delete existing post via /posts api', () => {
+    cy.request({
+      method: 'DELETE',
+      url: 'http://localhost:3000/posts/13',
+    }).then((response) => {
+      expect(response.status).to.eql(200);
+    });
+  });
+});
